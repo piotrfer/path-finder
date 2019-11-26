148c3887eaf40ff996f004f967a449dcdf3ee5be
piotrfer <piotrek99f@gmail.com>
2019-11-26 21:29:20 +0100

    0.3.6 obsługa grafów niespójnych, poprawa sumowania ceny

--- project/src/source/Map.java
+++ project/src/source/Map.java
@@ -18,62 +18,95 @@ public class Map {
         double[][] priceMatrix = this.priceMatrix;
         ArrayList<String>[][] pointsThrough = new ArrayList[timeMatrix.length][timeMatrix.length];
 
-        for (int i = 0; i < timeMatrix.length; i++) {
+        for (int start = 0; start < timeMatrix.length; start++) {
             ArrayList<String> queue = (ArrayList<String>) allPlaces.clone();
             ArrayList<String> seen = new ArrayList<>();
 
-            int[] currentPointArray = timeMatrix[i];
+            int[] byTimeArray = timeMatrix[start];
             while (queue.size() != 0) {
-                int minimumIndex = getMinimumIndex(currentPointArray, seen, allPlaces);
-                queue.remove(allPlaces.get(minimumIndex));
-                seen.add(allPlaces.get(minimumIndex));
+                int by = getMinimumIndex(byTimeArray, queue, allPlaces);
+                queue.remove(allPlaces.get(by));
+                seen.add(allPlaces.get(by));
 
-                currentPointArray = timeMatrix[minimumIndex];
-                for (int j = 0; j < currentPointArray.length; j++) {
-                    if (currentPointArray[j] == Integer.MAX_VALUE) {
+                byTimeArray = timeMatrix[by];
+                for (int finish = 0; finish < byTimeArray.length; finish++) {
+                    if (byTimeArray[finish] == Integer.MAX_VALUE || timeMatrix[start][by] == Integer.MAX_VALUE) {
                         continue;
                     }
-                    if (timeMatrix[i][minimumIndex] + currentPointArray[j] < timeMatrix[i][j]) {//jeżeli droga przez dany wierzchołek jest mniejsza
-                        timeMatrix[i][j] = timeMatrix[i][minimumIndex] + currentPointArray[j];
-                        pointsThrough[i][j] = new ArrayList<>();
-                        if (pointsThrough[i][minimumIndex] != null) {
-                            pointsThrough[i][j].addAll(pointsThrough[i][minimumIndex]);
-                        }
-                        pointsThrough[i][j].add(allPlaces.get(minimumIndex));
-                        if (pointsThrough[minimumIndex][j] != null) {
-                            pointsThrough[i][j].addAll(pointsThrough[minimumIndex][j]);
+                    int timeBy = timeMatrix[start][by] + byTimeArray[finish];
+                    double priceBy = priceMatrix[start][by] + priceMatrix[by][finish];
+                    if ( timeBy < timeMatrix[start][finish] || (timeBy == timeMatrix[start][finish] && priceBy < priceMatrix[start][finish])) {//jeżeli droga przez dany wierzchołek jest mniejsza
+                        System.out.println(allPlaces.get(start) + " | " + allPlaces.get(finish) + " | " + allPlaces.get(start) + "->" + allPlaces.get(by) + "->" + allPlaces.get(finish) + " " + (timeBy) + " AKTUALNA NAJKRÓTSZA DROGA: " + timeMatrix[start][finish]);
+
+                        timeMatrix[start][finish] = timeBy;
+                        pointsThrough[start][finish] = new ArrayList<>();
+
+                        if (pointsThrough[start][by] != null) {
+                            pointsThrough[start][finish].addAll(pointsThrough[start][by]);
                         }
 
-                        System.out.println(allPlaces.get(i) + " | " + allPlaces.get(minimumIndex) + " | " + allPlaces.get(i) + "->" + allPlaces.get(minimumIndex) + " " + (timeMatrix[i][minimumIndex]) + " | " + allPlaces.get(i) + "->" + allPlaces.get(j) + " " + (timeMatrix[i][minimumIndex] + currentPointArray[j]) + " AKTUALNA NAJKRÓTSZA DROGA: " + timeMatrix[i][j]);
-                        if (pointsThrough[i][j] != null) {
-                            System.out.println(pointsThrough[i][j]);
+                        pointsThrough[start][finish].add(allPlaces.get(by));
+
+                        if (pointsThrough[by][finish] != null) {
+                            pointsThrough[start][finish].addAll(pointsThrough[by][finish]);
+                        }
+
+                        priceMatrix[start][finish] = priceBy;
+
+                        if (pointsThrough[start][finish] != null) {
+                            System.out.println(pointsThrough[start][finish]);
                         } else {
                             System.out.println("[]");
                         }
                     }
-
                 }
             }
         }
         this.pointsThroughMatrix = pointsThrough;
-        return true;
+
+        if( isGraphUncompleted(timeMatrix, allPlaces) ){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
-    private int getMinimumIndex(int[] array, ArrayList<String> seen, ArrayList<String> allPoints) {
+    private boolean isGraphUncompleted( int[][] timeMatrix, ArrayList<String> allPlaces ){
+        for(int i = 0; i < timeMatrix.length; i++){
+            for(int j = 0; j < timeMatrix[i].length; j++){
+                if( timeMatrix[i][j] == Integer.MAX_VALUE && timeMatrix[j][i] == Integer.MAX_VALUE){
+                    System.err.println("Graf jest niespójny. Wierzchołek " + allPlaces.get(j) + " nie jest połączony z innymi.");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int getMinimumIndex(int[] array, ArrayList<String> queue, ArrayList<String> allPoints) {
         if (array == null || array.length == 0) {
             return -1;
         } else {
             int minValue = Integer.MAX_VALUE;
             int minIndex = -1;
             for (int i = 0; i < array.length; i++) {
-                if (seen.contains(allPoints.get(i)))
+                if (!queue.contains(allPoints.get(i)))
                     continue;
                 if (minValue > array[i]) {
                     minValue = array[i];
                     minIndex = i;
                 }
             }
-            return minIndex != -1 ? minIndex : !seen.isEmpty() ? allPoints.indexOf(seen.get(0)) : -1;
+            if( minIndex != -1 ){
+                return minIndex;
+            }
+            else if( !queue.isEmpty() ){
+                return allPoints.indexOf(queue.get(0));
+            }
+            else{
+                return -1;
+            }
         }
     }
 
@@ -84,8 +117,8 @@ public class Map {
         ArrayList<String>[][] npointsThrough = new ArrayList[size][size];
         ArrayList<Integer> common = new ArrayList<>();
 
-        for(int i = 0; i < allPlaces.size(); i++){
-            if( chosenPoints.contains(allPlaces.get(i))){
+        for (int i = 0; i < allPlaces.size(); i++) {
+            if (chosenPoints.contains(allPlaces.get(i))) {
                 common.add(i);
             }
         }
@@ -93,8 +126,8 @@ public class Map {
         int n = 0;
         int m = 0;
 
-        for( int i : common ){
-            for( int j : common){
+        for (int i : common) {
+            for (int j : common) {
                 ntimeMatrix[n][m] = this.timeMatrix[i][j];
                 npriceMatrix[n][m] = this.priceMatrix[i][j];
                 npointsThrough[n][m] = this.pointsThroughMatrix[i][j];
