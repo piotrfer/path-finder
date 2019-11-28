f89f4ab7c08a0ded04e5d98dc9e57ce89eb2ed69
piotrfer <piotrek99f@gmail.com>
2019-11-28 08:12:40 +0100

    1.0.0 wersja ostateczna

--- project/src/source/PathFinder.java
+++ project/src/source/PathFinder.java
@@ -3,19 +3,19 @@ package source;
 public class PathFinder {
 
     public static void main(String[] args) {
-        Setup mountainSetup;
-        if ((mountainSetup = Reader.setUpData(args)) == null) {
+        Setup setup;
+        if ((setup = Reader.setUpData(args)) == null) {
             System.exit(1);
         }
-        if (!mountainSetup.getReady()) {
+        if (!setup.getReady()) {
             System.exit(1);
         }
         Solution s;
         Solver heldKarp = new Solver();
-        if ((s = heldKarp.solveMap(mountainSetup)) == null) {
+        if ((s = heldKarp.solveMap(setup)) == null) {
             System.exit(1);
         }
-        if (!WriteOnScreen.showPath(s, mountainSetup)) {
+        if (!WriteOnScreen.showPath(s, setup)) {
             System.exit(1);
         }
     }
